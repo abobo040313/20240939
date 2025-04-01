d0d806624dd695f284da8fe3e0a2fe73bcf8829b
abobo040313 <abobo040313@naver.com>
2025-04-01 21:35:38 +0900

    Update 20240939_stack_report1.c

--- 20240939_stack_report1.c
+++ 20240939_stack_report1.c
@@ -1,22 +1,18 @@
 int main() {
-    char input[MAX_LENGTH + 1];
     Stack s;
-    initStack(&s, MAX_LENGTH);
+    char str[MAX];
+    initStack(&s);
 
-    printf("문자열을 입력하세요 (최대 %d자): ", MAX_LENGTH);
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0'; 
+    printf("문자열 입력 : ");
+    fgets(str, MAX, stdin);
+    str[strcspn(str, "\n")] = '\0';
 
-    for (int i = 0; input[i] != '\0'; i++) {
-        push(&s, input[i]);
-    }
-
-    printf("거꾸로 출력된 문자열: ");
-    while (!isEmpty(&s)) {
+    for(int i = 0; str[i] != '\0'; i++) push(&s, str[i]);
+    printf("거꾸로 된 문자열 : ");
+    while(!isEmpty(&s)) {
         printf("%c", pop(&s));
     }
     printf("\n");
 
-    freeStack(&s);
     return 0;
 }
