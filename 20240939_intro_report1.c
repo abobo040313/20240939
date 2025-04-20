d49f6373963a29b57f56e054bfffbf2498f64f5f
abobo040313 <abobo040313@naver.com>
2025-04-20 14:01:46 +0900

    20240939_intro_report1.c

--- 20240939_intro_report1.c
+++ 20240939_intro_report1.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 100
 int main()
 {
-    int number[SIZE];
+    int number[10];
     int total = 0;
-    int max, min;
+    int max,min;
     float ovg = 0.0;
     srand(time(NULL));
-    for(int i=0; i<SIZE; i++){
+    for(int i=0; i<10; i++){
         number[i] = (rand() % 100) + 1;
+    }   
+    for(int i=0; i<10; i++){
+        printf("%3d\t",number[i]);
+        if((i+1) % 5 == 0) printf("\n");
+        
     }
-    for(int i=0; i<SIZE; i++){
-        printf("%3d\t", number[i]);
-        if((i+1) % 10 == 0) printf("\n");
-    }
-    for(int i=0; i<SIZE; i++){
+    for(int i=0; i<10; i++){
         total += number[i];
     }
-    ovg = (float)total / SIZE;
-    
-    max = number[0];
-    min = number[0];
-    
-    for(int i=1; i<SIZE; i++){
-        if(max < number[i]) max = number[i];
-        if(min > number[i]) min = number[i];
-    }
-    printf("ovg = %f, max = %d, min = %d\n",ovg, max, min);
+    ovg = (float)total / 10;
+    printf("%f", ovg);
     return 0;
-}
-
     
+}
